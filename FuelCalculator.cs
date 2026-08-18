35fdd10abb85ce9dd55f81f3d3791ba603732ac1
Githmi26 <104828196@student.swin.edu.au>
2026-08-18 23:57:35 +1000

    Add files via upload

---
+++ FuelCalculator.cs
@@ -0,0 +1,27 @@
+﻿namespace FuelMathLibrary
+{
+    public static class FuelCalculator
+    {
+        public static double CalculateFuelRequired(
+           double distance,
+           double consumption)
+        {
+            return (distance / 100) * consumption;
+        }
+
+        public static double CalculateTotalCost(
+            double fuelRequired,
+            double fuelPrice)
+        {
+            return fuelRequired * fuelPrice;
+        }
+
+        public static double CalculateCostPerPerson(
+            double totalCost,
+            int passengers)
+        {
+            return totalCost / passengers;
+        }
+
+    }
+}
---
+++ TripValidator.cs
@@ -0,0 +1,20 @@
+﻿namespace TripValidationLibrary
+{
+    public static class TripValidator
+    {
+        public static bool ArePositiveValues(
+            double distance,
+            double consumption,
+            double fuelPrice)
+        {
+            return distance > 0 &&
+                   consumption > 0 &&
+                   fuelPrice > 0;
+        }
+
+        public static bool IsPassengerCountValid(int passengers)
+        {
+            return passengers > 0;
+        }
+    }
+}
